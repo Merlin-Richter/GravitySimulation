a628ecad85b2bf244acd98149ae7d7864bc1e310
LukasFink04 <lukas.fink04@gmail.com>
2025-07-07 17:27:57 +0200

    Berechnung der gravitationskraft ist drinne das müsste stimmen, ich habe bei dem update von v und x ein dt reingemacht das müsste man in main iwie definieren also des soll halt die Zeit sein.

--- src/Planet.java
+++ src/Planet.java
@@ -5,30 +5,45 @@ import java.awt.*;
 
 public  class Planet implements Entity {
 
-        double x, y, vx, vy, ax, ay, m, r;
+    double x, y, vx, vy, ax, ay, m, r;
         Color color;
         public Planet (double x , double y , double vx, double vy, double m, double r, Color color){
             this.x = x; this.y = y ; this.vx = vx; this.vy = vy; this.m = m; this.r = r; this.color = color;
         }
 
-        public void update(double dt) {
+    @Override
+    public double get_x() {
+        return this.x;
+    }
+
+    @Override
+    public double get_m() {
+        return this.m;
+    }
+
+    @Override
+    public double get_y() {
+        return this.y;
+    }
+
+    public void update(double dt) {
             vx += ax * dt;
             vy += ay * dt ;
             x+= vx * dt;
             y += vy * dt;
         }
 
-        public void force_calc (Planet other){
+        public void force_calc (Entity other){
             double G = 1;
             double FGx = 0;
             double FGy = 0;
             double FG = 0;
-            double x_distance = other.x - x;
-            double y_distance = other.y - y;
+            double x_distance = other.get_x() - x;
+            double y_distance = other.get_y() - y;
             double r2 = Math.pow(x_distance,2) + Math.pow(y_distance,2);
             double r = Math.sqrt(r2);
             if (r2 == 0) return;
-            FG = G * (m * other.m )/ r2;
+            FG = G * (m * other.get_m() )/ r2;
             FGx = FG * x_distance/r;
             FGy = FG * y_distance/r;
             ax += FGx/m;
